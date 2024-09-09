be39909e1d43ebfaef3323cc6a7a5434174931ab
mortenl-dev <mortenlins@gmail.com>
2024-09-09 16:16:38 +0200

    First prototype

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,71 @@
-﻿using System.Text;
+﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
-namespace windowsGithubBG;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace YourApp
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Set the window size to the screen's working area (excluding taskbar)
+            SetWindowToFitScreen();
+            // Make the window click-through
+            MakeWindowClickThrough();
+            // Set the window to be in the background behind everything else
+            SetWindowToDesktop();
+        }
+
+        private void SetWindowToFitScreen()
+        {
+            // Get the working area (excluding taskbar)
+            var workingArea = SystemParameters.WorkArea;
+
+            // Set the window size to fit the working area
+            this.Left = workingArea.Left;
+            this.Top = workingArea.Top;
+            this.Width = workingArea.Width;
+            this.Height = workingArea.Height;
+        }
+
+        private void SetWindowToDesktop()
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_TOOLWINDOW);
+            SetWindowPos(hwnd, HWND_BOTTOM, 0, 0, (int)Width, (int)Height, SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE);
+        }
+
+        private void MakeWindowClickThrough()
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+
+        // PInvoke declarations for Windows API
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_LAYERED = 0x00080000;
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int WS_EX_TOOLWINDOW = 0x00000080;
+        const int SWP_NOACTIVATE = 0x0010;
+        const int SWP_NOMOVE = 0x0002;
+        const int SWP_NOSIZE = 0x0001;
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
     }
 }
